63d7f58e5ed12f554478a856becd35122d1c5126
Lucas Brazzarola <69698754+LucasBrazza@users.noreply.github.com>
2022-10-01 16:54:42 -0300

    updated pc incrementor

---
+++ Processador/AddPC4.v
@@ -0,0 +1,18 @@
+module AddPC4(clock, PC, PC4, PCWrite);
+
+input clock;
+input [15:0]PC;
+input PCWrite;
+output [15:0]PC4;
+reg PC4;
+
+always @ (*)begin
+  while (PCWrite == 0) begin
+    PC4 = PC;
+  end
+  if (PCWrite == 1) begin
+    PC4 = PC + 16'b0000000000000100;
+  end
+end
+
+endmodule
