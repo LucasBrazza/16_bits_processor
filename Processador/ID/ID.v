3e0602558f278285a30872e9b2274729c40e72ba
Lucas Brazzarola <69698754+LucasBrazza@users.noreply.github.com>
2022-10-17 15:00:10 -0300

    tested ID module

--- Processador/ID/ID.v
+++ Processador/ID/ID.v
@@ -1,7 +1,7 @@
-`include "RegisterBank.v"
-`include "SignalExtensor.v"
+`include "../Processador\ID\RegisterBank.v"
+`include "../Processador\ID\SignalExtensor.v"
 
-module ID(clock, rs, rt, rd, funct, address, addressjump, PC4, writeDat, writeRegister, RegWrite, readData1, 
+module ID(clock, rs, rt, rd, funct, addressjump, PC4, writeDat, RegWrite, readData1, 
             readData2, extendedSignal, PC4_outpuID);
 
     input clock;
@@ -10,54 +10,32 @@ module ID(clock, rs, rt, rd, funct, address, addressjump, PC4, writeDat, writeRe
     input [2:0]rs;
     input [2:0]rt;
     input [2:0]rd;
-    input [2:0]funct;
-    input [15:0]address;
-    input [13:0]addressjump;
+    input [1:0]funct;
+    input [7:0]addressjump;
     input [15:0]writeDat;
-    input [2:0]writeRegister;
 
     output [15:0]readData1;
     output [15:0]readData2;
     output [15:0]extendedSignal;
-
     output reg [15:0]PC4_outpuID;
 
     initial begin
         PC4_outpuID = PC4;
     end
 
-    RegsterBank bank(
+    RegisterBank bank(
         .clock(clock), 
         .RegWrite(RegWrite), 
         .reg1(rs), 
         .reg2(rt), 
         .reg3(rd), 
-        .dado_escrita(writeDat), 
-        .readData1(readData1), 
-        .readData2(readData2));
+        .dataToWrite(writeDat), 
+        .data1(readData1), 
+        .data2(readData2));
 
     SignalExtensor extensor(
         .clock(clock), 
-        .signal8(address), 
+        .signal8(addressjump), 
         .signal16(extendedSignal));
-
-    /*initial begin
-        $monitor("clock = %b \n registrador1 = %b \n registrador2 = %b \n registradordestino = %b \n dado escrito = %b \n dado 1 = %b, dado 2 = %b, sinal extendido = %b", clock, reg1, reg2, RegDst, writeDat, saida1, saida2, sinal32saida);
-        clock = 0;
-        EscReg = 0;
-        //#5 instuction = 32'b00000000001000100000000000000001;
-        #5 RegDst = 32'b00000;
-        //writeDat = 32'b00000000001000100000000000000000;
-        
-        #40 $finish;
-    end
-
-    always begin
-    #5 clock = ~clock;
-    end*/
-
-    //banco_registradores banco(.clock(clock), .RegWrite(EscReg), .r1(reg1), .r2(reg2), .r3(RegDst), .dado_escrita(writeDat), .dado_lido1(saida1), .dado_lido2(saida2));
-
-    //extensor16_32 extensor(.clock(clock), .sinal16(sinal16entrada), .sinal32(sinal32saida));
 
 endmodule
--- Processador/ID/RegisterBank.v
+++ Processador/ID/RegisterBank.v
@@ -9,26 +9,26 @@ module RegisterBank(clock, RegWrite, reg1, reg2, reg3, dataToWrite, data1, data2
 
     // esse é o output original, achei q saiam 32 bits, mas aqui saem só 5???
     // no nosso caso seria o que entao?
-    output reg [2:0]data1;
-    output reg [2:0]data2;
+    output reg [15:0]data1;
+    output reg [15:0]data2;
 
     // esse [5:0] tbm é o original, ele diz respeito a que?
-    reg [5:0]registers[15:0];
+    reg [2:0]registers[15:0];
 	 
     initial begin
-	registers[0] = 3'b000;
-	registers[1] = 3'b001;
-	registers[2] = 3'b010;
-	registers[3] = 3'b100;
-	registers[4] = 3'b011;
-	registers[5] = 3'b101;
-	registers[6] = 3'b110;
-	registers[7] = 3'b111;
+        registers[0] = 3'b000;
+        registers[1] = 3'b001;
+        registers[2] = 3'b010;
+        registers[3] = 3'b100;
+        registers[4] = 3'b011;
+        registers[5] = 3'b101;
+        registers[6] = 3'b110;
+        registers[7] = 3'b111;
     end
 
     always @ (posedge clock)begin
-      data1 = registers[reg1];
-      data2 = registers[reg2];
+        data1 = registers[reg1];
+        data2 = registers[reg2];
     end
 
     always @ (negedge clock)begin
