70d7843f81b6d024aa780560a3fb437e55f91857
Ayron Luigi de paiva <49703457+ImAyron@users.noreply.github.com>
2022-10-19 16:04:18 -0300

    .

--- Processador/IF/instructionMemory.v
+++ Processador/IF/instructionMemory.v
@@ -2,20 +2,14 @@ module InstructionMemory(address, instructionOutput, clock);
 
     input clock;
     input [15:0]address;
-
+    
     output reg [15:0]instructionOutput;
     
     reg [15:0]instructions;
 
     initial begin
-      $readmemb("instruções.txt", instructions);
+      instructionOutput=16'b0001110111000010;
     end
     
-    always @ (negedge clock) begin
-      case(address) begin
-        3'b001  : instructionOutput
-      end
-      instructionOutput = instructions[(address/4) - 1];
-    end
-
+    
 endmodule
